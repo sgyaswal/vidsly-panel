d9fa1b0fdfdffe64266f8ef136fb2887d0857a13
sgyaswal <sgyaswal@gmail.com>
2024-01-25 19:16:52 +0530

    sign up

--- src/scenes/signup/signup.js
+++ src/scenes/signup/signup.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Terms from "../../components/Terms/Terms";
 import TextInput from "../../components/TextInput/TextInput";
 import FullScroll from "./FullScroll";
@@ -10,113 +10,118 @@ import Loader from "../../components/Loader/Loader";
 import CompletionMessage from "../../components/CompletionMessage/CompletionMessage";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
+import rightArrow from "../../assets/right-arrow.svg";
+import ButtonContainer from "../../components/ButtonContainer/ButtonContainer";
+import ErrorContainer from "../../components/ErrorContainer/ErrorContainer";
+
 
 const questions = [
   {
     id: 1,
-    type: "text",
+    type: "first",
     text: "What is your first name?",
     isRequired: true,
   },
   {
     id: 2,
-    type: "text",
+    type: "second",
     text: "and what is your last name, {name}?",
     isRequired: true,
   },
   {
     id: 3,
-    type: "text",
+    type: "third",
     text: "Email you'd like to register with?",
     isRequired: true,
     subTitle:
       "We will keep all our communications with you through this email. Do check your spam inbox if you can't find our application received email.",
     placeholder: "name@example.com",
     validation: "email",
+    isLastQuestion:true
   },
-  {
-    id: 4,
-    type: "text",
-    text: "Your phone number",
-    isRequired: true,
-    subTitle:
-      "We won't call you unless it is absolutely required to process your application.",
-    placeholder: "089621 8845",
-    validation: "phone",
-  },
-  {
-    id: 5,
-    type: "text",
-    text: "Create a password",
-    isRequired: true,
-    subTitle: "Make sure it's at least 8 characters long.",
-    placeholder: "********",
-    validation: "password",
-  },
-  {
-    id: 6,
-    type: "text",
-    text: "Confirm your password",
-    isRequired: true,
-    subTitle: "Enter the same password as above.",
-    placeholder: "********",
-    validation: "confirmPassword",
-  },
-  {
-    id: 7,
-    type: "radio",
-    text: "What is Your Gender?",
-    isRequired: false,
-    options: ["Male", "Female", "Other"],
-  },
-  {
-    id: 8,
-    type: "text",
-    text: "Country",
-    isRequired: false,
-  },
-  {
-    id: 9,
-    type: "text",
-    text: "City",
-    isRequired: false,
-  },
-  {
-    id: 10,
-    type: "radio-group",
-    text: "I am",
-    isRequired: true,
-    options: [
-      "Content Creator",
-      "Artist",
-      "Vlogger",
-      "Gamer",
-      "Brand",
-      "Public figure",
-      "Influrncer",
-      "Developer/Designer",
-      "Entrepreneur",
-      "Chef",
-      "Publisher",
-      "Other",
-    ],
-  },
-  {
-    id: 11,
-    type: "radio",
-    text: "I make content related to",
-    isRequired: true,
-    options: [
-      "Sports",
-      "Food",
-      "Photography",
-      "Business",
-      "Music",
-      "Entertainment",
-      "Other",
-    ],
-    isLastQuestion: true,
-  },
+  // {
+  //   id: 4,
+  //   type: "text",
+  //   text: "Your phone number",
+  //   isRequired: true,
+  //   subTitle:
+  //     "We won't call you unless it is absolutely required to process your application.",
+  //   placeholder: "089621 8845",
+  //   validation: "phone",
+  // },
+  // {
+  //   id: 5,
+  //   type: "text",
+  //   text: "Create a password",
+  //   isRequired: true,
+  //   subTitle: "Make sure it's at least 8 characters long.",
+  //   placeholder: "********",
+  //   validation: "password",
+  // },
+  // {
+  //   id: 6,
+  //   type: "text",
+  //   text: "Confirm your password",
+  //   isRequired: true,
+  //   subTitle: "Enter the same password as above.",
+  //   placeholder: "********",
+  //   validation: "confirmPassword",
+  // },
+  // {
+  //   id: 7,
+  //   type: "radio",
+  //   text: "What is Your Gender?",
+  //   isRequired: false,
+  //   options: ["Male", "Female", "Other"],
+  // },
+  // {
+  //   id: 8,
+  //   type: "text",
+  //   text: "Country",
+  //   isRequired: false,
+  // },
+  // {
+  //   id: 9,
+  //   type: "text",
+  //   text: "City",
+  //   isRequired: false,
+  // },
+  // {
+  //   id: 10,
+  //   type: "radio-group",
+  //   text: "I am",
+  //   isRequired: true,
+  //   options: [
+  //     "Content Creator",
+  //     "Artist",
+  //     "Vlogger",
+  //     "Gamer",
+  //     "Brand",
+  //     "Public figure",
+  //     "Influrncer",
+  //     "Developer/Designer",
+  //     "Entrepreneur",
+  //     "Chef",
+  //     "Publisher",
+  //     "Other",
+  //   ],
+  // },
+  // {
+  //   id: 11,
+  //   type: "radio",
+  //   text: "I make content related to",
+  //   isRequired: true,
+  //   options: [
+  //     "Sports",
+  //     "Food",
+  //     "Photography",
+  //     "Business",
+  //     "Music",
+  //     "Entertainment",
+  //     "Other",
+  //   ],
+  //   isLastQuestion: true,
+  // },
 ];
 
 const isValidEmail = (email) => {
@@ -141,6 +146,14 @@ function SignUp() {
   const [questionNumber, setQuestionNumber] = useState(1);
   const [loading, setLoading] = useState(true);
   const [submitForm, setSubmitForm] = useState(false);
+  const inputRef = useRef(null);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfimPassword] = useState("");
+
+  console.log(firstName,lastName,email,password,confirmPassword)
 
   const handleSignup = async () => {
     try {
@@ -148,8 +161,8 @@ function SignUp() {
         first_name: answers.find((a) => a.id === 1)?.value || "",
         last_name: answers.find((a) => a.id === 2)?.value || "",
         email: answers.find((a) => a.id === 3)?.value || "",
-        phone: answers.find((a) => a.id === 4)?.value ||"",
-        password: answers.find((a) => a.id == 5)?.value || ""
+        phone: answers.find((a) => a.id === 4)?.value || "",
+        password: answers.find((a) => a.id == 5)?.value || "",
       };
       const response = await fetch(
         `${process.env.REACT_APP_BACKEND_URL}/api/user/register`,
@@ -256,19 +269,210 @@ function SignUp() {
     }
 
     switch (question.type) {
-      case "text":
+      case "first":
         return (
-          <TextInput
-            error={error}
-            answers={answers}
-            key={question.id}
-            question={question}
-            onAnswer={handleAnswer}
-            showError={showError}
-            questionText={questionText}
-            updateCurrentQuestionId={updateCurrentQuestionId}
-            questionNumber={questionNumber}
-          />
+          <>
+            <div
+              className="question-container"
+              // id={question.validation === "phone" ? "text-input" : ""}
+            >
+              <div className="question-number-container">
+                <label className="question-text">
+                  {"First Name"}
+                </label>
+              </div>
+              <div className="question-subtitle">
+              </div>
+              <div>
+                <input
+                  // ref={inputRef}
+                  className="text-input"
+                  type="text"
+                  id="FirstName"
+                  onChange={(e) => setFirstName(e.target.value)}
+                  placeholder="First Name"
+                  value={firstName}
+                  required
+                />
+              </div>
+
+              {/* second input field */}
+              <div className="question-number-container">
+                <label className="question-text">
+                  {"Last Name"}
+                </label>
+              </div>
+              
+              <div>
+                <input
+                  // ref={inputRef}
+                  className="text-input"
+                  type="text"
+                  id="LastName"
+                  onChange={(e) => setLastName(e.target.value)}
+                  placeholder="Last Name"
+                  value={lastName}
+                />
+              </div>
+              <div className="question-number-container">
+                <label className="question-text">
+                  {"Email"}
+                </label>
+              </div>
+              
+              <div>
+                <input
+                  // ref={inputRef}
+                  className="text-input"
+                  type="text"
+                  id="Email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email"
+                  
+                  value={email}
+                />
+              </div>
+              <div className="question-number-container">
+                <label className="question-text">
+                  {"Password"}
+                </label>
+              </div>
+              
+              <div>
+                <input
+                  // ref={inputRef}
+                  className="text-input"
+                  type="password"
+                  id="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                  
+                  value={password}
+                />
+              </div>
+              <div className="question-number-container">
+                <label className="question-text">
+                  {"Confirm Password"}
+                </label>
+              </div>
+              
+              <div>
+                <input
+                  // ref={inputRef}
+                  className="text-input"
+                  type="password"
+                  id="Confirmpassword"
+                  onChange={(e) => setConfimPassword(e.target.value)}
+                  placeholder="Confirm Password"
+                  
+                  value={confirmPassword}
+                />
+              </div>
+              
+              <div>{showError && <ErrorContainer error={error} />}</div>
+              <div>
+                {!showError && (
+                  <ButtonContainer
+                    buttonText={
+                      question.isLastQuestion === true ? "Submit" : "OK"
+                    }
+                    showPressEnterText={
+                      question.isLastQuestion === true
+                        ? "Ctrl + Enter"
+                        : "Enter"
+                    }
+                    showPressEnter={true}
+                    // handleButtonClick={updateNextPage}
+                  />
+                )}
+              </div>
+            </div>
+          </>
+        );
+        case "second":
+        return (
+          <>
+            <div
+              className="question-container"
+              // id={question.validation === "phone" ? "text-input" : ""}
+            >
+              <div className="question-number-container">
+                <label className="question-text">
+                  {"First Name"}
+                </label>
+              </div>
+              <div className="question-subtitle">
+              </div>
+              <div>
+                <input
+                  // ref={inputRef}
+                  className="text-input"
+                  type="text"
+                  id="FirstName"
+                  onChange={(e) => setFirstName(e.target.value)}
+                  placeholder="First Name"
+                  
+                  value={firstName}
+                />
+              </div>
+
+              {/* second input field */}
+              <div className="question-number-container">
+                <label className="question-text">
+                  {"Last Name"}
+                </label>
+              </div>
+              
+              <div>
+                <input
+                  // ref={inputRef}
+                  className="text-input"
+                  type="text"
+                  id="LastName"
+                  onChange={(e) => setLastName(e.target.value)}
+                  placeholder="Last Name"
+                  value={lastName}
+                />
+              </div>
+              <div className="question-number-container">
+                <label className="question-text">
+                  {"Email"}
+                </label>
+              </div>
+              
+              <div>
+                <input
+                  // ref={inputRef}
+                  className="text-input"
+                  type="text"
+                  id="Email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email"
+                  
+                  value={email}
+                />
+              </div>
+              
+              
+              <div>{showError && <ErrorContainer error={error} />}</div>
+              <div>
+                {!showError && (
+                  <ButtonContainer
+                    buttonText={
+                      question.isLastQuestion === true ? "Submit" : "OK"
+                    }
+                    showPressEnterText={
+                      question.isLastQuestion === true
+                        ? "Ctrl + Enter"
+                        : "Enter"
+                    }
+                    showPressEnter={true}
+                    // handleButtonClick={updateNextPage}
+                  />
+                )}
+              </div>
+            </div>
+          </>
         );
       case "select":
         return (
@@ -335,73 +539,73 @@ function SignUp() {
     setAnswers([...newAnswers]);
   };
 
-  const isRequiredQuestionAnswered = () => {
-    let currentQuestion = questions.find((q) => q.id === currentQuestionId);
-    let currentAnswer = answers.find((a) => a.id === currentQuestionId);
-    if (currentQuestion.isRequired !== true) return true;
+  // const isRequiredQuestionAnswered = () => {
+  //   let currentQuestion = questions.find((q) => q.id === currentQuestionId);
+  //   let currentAnswer = answers.find((a) => a.id === currentQuestionId);
+  //   if (currentQuestion.isRequired !== true) return true;
 
-    if (currentAnswer) {
-      if (currentQuestion.type === "radio-group") {
-        if (currentAnswer.value === null || currentAnswer.value.length === 0) {
-          handleSetError("Oops! Please make a selection");
+  //   if (currentAnswer) {
+  //     if (currentQuestion.type === "radio-group") {
+  //       if (currentAnswer.value === null || currentAnswer.value.length === 0) {
+  //         handleSetError("Oops! Please make a selection");
 
-          removeAnswer();
+  //         removeAnswer();
 
-          return false;
-        } else if (currentAnswer.value.length < currentQuestion.maxSelect) {
-          handleSetError("Please make more choices");
-          return false;
-        }
-      } else if (
-        currentAnswer.value === null ||
-        currentAnswer.value.length === 0
-      ) {
-        if (currentQuestion.type === "text") {
-          handleSetError("Please fill this in");
-        } else {
-          handleSetError("Please make a selection");
-        }
+  //         return false;
+  //       } else if (currentAnswer.value.length < currentQuestion.maxSelect) {
+  //         handleSetError("Please make more choices");
+  //         return false;
+  //       }
+  //     } else if (
+  //       currentAnswer.value === null ||
+  //       currentAnswer.value.length === 0
+  //     ) {
+  //       if (currentQuestion.type === "text") {
+  //         handleSetError("Please fill this in");
+  //       } else {
+  //         handleSetError("Please make a selection");
+  //       }
 
-        removeAnswer();
-        return false;
-      }
+  //       removeAnswer();
+  //       return false;
+  //     }
 
-      if (currentQuestion.validation === "email") {
-        if (isValidEmail(currentAnswer.value)) {
-          return true;
-        }
+  //     if (currentQuestion.validation === "email") {
+  //       if (isValidEmail(currentAnswer.value)) {
+  //         return true;
+  //       }
 
-        handleSetError("Hmmm... that email doesn't look right");
-        return false;
-      }
+  //       handleSetError("Hmmm... that email doesn't look right");
+  //       return false;
+  //     }
 
-      if (currentQuestion.validation === "phone") {
-        if (isValidPhoneNumber(currentAnswer.value)) {
-          return true;
-        }
+  //     if (currentQuestion.validation === "phone") {
+  //       if (isValidPhoneNumber(currentAnswer.value)) {
+  //         return true;
+  //       }
 
-        handleSetError("Hmmm... that phone number doesn't look right");
-        return false;
-      }
+  //       handleSetError("Hmmm... that phone number doesn't look right");
+  //       return false;
+  //     }
 
-      return true;
-    }
+  //     return true;
+  //   }
 
-    if (currentQuestion.type === "text") {
-      handleSetError("Please fill this in");
-    } else {
-      handleSetError("Please make a selection");
-    }
-    return false;
-  };
+  //   if (currentQuestion.type === "text") {
+  //     handleSetError("Please fill this in");
+  //   } else {
+  //     handleSetError("Please make a selection");
+  //   }
+  //   return false;
+  // };
 
-  const handleSetError = (newError) => {
-    setError(newError);
-  };
+  // const handleSetError = (newError) => {
+  //   setError(newError);
+  // };
 
   const checkIfLastQuestion = () => {
     let currentQuestion = questions.find((q) => q.id === currentQuestionId);
-    if (currentQuestion.isLastQuestion && isRequiredQuestionAnswered()) {
+    if (currentQuestion.isLastQuestion) {
       setSubmitForm(true);
     }
 
@@ -413,12 +617,12 @@ function SignUp() {
   }
 
   if (submitForm) {
-    handleSignup();
+    // handleSignup();
   }
   return (
     <>
       <FullScroll
-        isRequiredQuestionAnswered={isRequiredQuestionAnswered}
+        // isRequiredQuestionAnswered={isRequiredQuestionAnswered}
         answers={answers}
         handleShowError={handleShowError}
         checkIfLastQuestion={checkIfLastQuestion}
