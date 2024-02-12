c8561e60b25cae8315be3a1e7c9c09a9e130ea6b
AnshMittal12 <anshmittal9888@gmail.com>
2024-02-13 00:28:45 +0530

    change

--- src/scenes/signup/logiin.js
+++ src/scenes/signup/logiin.js
@@ -605,7 +605,7 @@ export default function Logiin({ firstName, lastName, email, password, confirmPa
             sx={{
               // marginTop: 10,
               // backgroundColor:'#000',
-              height: '98vh',
+              height: '95vh',
               display: 'flex',
               flexDirection: 'column',
               alignItems: 'center',
